fe88aa3aa412124d8af9927550ac7c428d802161
tetoraillia <teterailiya@gmail.com>
2025-07-04 11:06:44 +0300

    feat: add checks for user login and if user instructor

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,7 @@
 class CoursesController < ApplicationController
     before_action :set_course, only: [ :show, :edit, :update, :destroy ]
+    before_action :authenticate_user!, except: [ :index, :show ]
+    before_action :check_instructor, only: [ :edit, :update, :destroy ]
 
     def index
         @courses = Course.all
@@ -45,5 +47,11 @@ class CoursesController < ApplicationController
 
     def course_params
         params.require(:course).permit(:title, :description, :instructor_id)
+    end
+
+    def check_instructor
+        unless current_user == @course.instructor
+            redirect_to courses_url, notice: "You are not authorized to perform this action."
+        end
     end
 end
