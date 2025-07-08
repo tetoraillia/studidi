fe2ab917f8cbf2a025a278af50c7e23869359bc9
tetoraillia <teterailiya@gmail.com>
2025-07-08 12:57:00 +0300

    style: Decide merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
   root "courses#index"
-
   devise_for :users, controllers: {
     sessions: "users/sessions",
     registrations: "users/registrations",
     passwords: "users/passwords",
     confirmations: "users/confirmations"
   }
-
   get "up" => "rails/health#show", as: :rails_health_check
-
   resources :courses do
     member do
       get :invite
@@ -20,13 +17,22 @@ Rails.application.routes.draw do
         get :accept, to: "invitations#accept"
       end
     end
-    
-    resources :course_modules do
-     resources :lessons do
-      collection do
-        get "select_lesson_type"
+    member do
+      get :invite
+    end
+    resources :enrollments, only: [ :create, :destroy ]
+    resources :invitations, only: [ :new, :create ] do
+      member do
+        get :accept, to: "invitations#accept"
       end
-     end
+    end
+    resources :course_modules do
+      resources :lessons
+      resources :lessons do
+        collection do
+          get "select_lesson_type"
+        end
+      end
     end
   end
 end
