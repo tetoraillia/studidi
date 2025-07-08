fb9b5084ec2112a5885205d98a404e859b507bda
tetoraillia <teterailiya@gmail.com>
2025-07-08 11:54:09 +0300

    feat: Add video validation if type is video

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,6 +2,11 @@ class Lesson < ApplicationRecord
   belongs_to :course_module
 
   validates :title, valid_characters: true, presence: true, length: { minimum: 5, maximum: 50 }
-  validates :content, valid_characters: true, presence: true, length: { minimum: 10 }
+  validates :content, valid_characters: true, presence: true, length: { minimum: 10 }, unless: -> { content_type == "video" }
+  validates :video_url, presence: true, if: -> { content_type == "video" }
   validates :course_module, presence: true
+
+  def content_required?
+    content_type != "video"
+  end
 end
