713f4c81d4e787ccca9fe7b1d1c88898e90f74a8
tetoraillia <teterailiya@gmail.com>
2025-07-10 15:14:40 +0300

    feat: Add scopes for topic model

--- app/models/Topic.rb
+++ app/models/Topic.rb
@@ -1,6 +1,9 @@
 class Topic < ApplicationRecord
   belongs_to :course
-  has_many :lessons, dependent: :destroy
+  has_many :lessons, -> { order(:position) }, dependent: :destroy
+
+  scope :for_course, ->(course_id) { where(course_id: course_id) }
+  scope :ordered, -> { order(created_at: :asc) }
 
   validates :title, valid_characters: true, presence: true, length: { minimum: 5, maximum: 50 }
   validates :course, presence: true
