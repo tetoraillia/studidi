394256d7140b82f8020986b63dda84f4860a8989
tetoraillia <teterailiya@gmail.com>
2025-07-11 13:01:11 +0300

    test: Add rspec tests for User model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,17 +1,18 @@
 FactoryBot.define do
   factory :user do
-    email { Faker::Internet.unique.email }
-    password { 'password123' }
     first_name { Faker::Name.first_name }
-    last_name  { Faker::Name.last_name }
-    role { :student }
-
-    trait :student do
-      role { :student }
-    end
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.unique.email }
+    password { Faker::Internet.password(min_length: 8) }
+    password_confirmation { |u| u.password }
+    encrypted_password { "some_hashed_password_for_tests" }
 
     trait :teacher do
-      role { :teacher }
+      role { "teacher" }
+    end
+
+    trait :student do
+      role { "student" }
     end
   end
 end
