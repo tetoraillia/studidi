995502a217236eb596a2f78d9c87d033f1d10138
tetoraillia <teterailiya@gmail.com>
2025-07-11 14:22:15 +0300

    test: Add tests for topics controller

--- spec/factories/topics.rb
+++ spec/factories/topics.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :topic do
     title { Faker::Lorem.words(number: 2).join(' ').capitalize }
     course { association(:course) }
-    position { Faker::Number.between(from: 1, to: 10) }
+    position { 1 }  
   end
 end
