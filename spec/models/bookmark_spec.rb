34139f339d561d59749dce44a91b094c047abdee
tetoraillia <teterailiya@gmail.com>
2025-07-14 12:22:13 +0300

    test: Add tests for bookmarks

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Bookmark, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:course) { create(:course) }
+
+  describe 'validations' do
+    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:course_id) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:course) }
+  end
 end
