26d26ba435cd45953074759821ad1e2dadee44f0
tetoraillia <teterailiya@gmail.com>
2025-07-14 12:05:38 +0300

    feat: Add bookmark's scopes

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,8 @@ class Bookmark < ApplicationRecord
   belongs_to :user
   belongs_to :course
 
+  scope :by_user, ->(user) { where(user_id: user.id) }
+
   validates :user_id, presence: true
   validates :course_id, presence: true,
     uniqueness: { scope: :user_id,
