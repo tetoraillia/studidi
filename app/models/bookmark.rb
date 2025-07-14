016fbed652707a6830b0d1586dc62d11d3037c84
tetoraillia <teterailiya@gmail.com>
2025-07-14 11:25:34 +0300

    feat: Add validates for Bookmark model

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,9 @@
 class Bookmark < ApplicationRecord
   belongs_to :user
   belongs_to :course
+
+  validates :user_id, presence: true
+  validates :course_id, presence: true,
+    uniqueness: { scope: :user_id,
+      message: "You have already bookmarked this course." }
 end
