952b8b0075f7b2159bed9402d8530f43cb1a3659
tetoraillia <teterailiya@gmail.com>
2025-07-15 13:51:11 +0300

    feat: Create response controller and set organizer for setting user_id for mark

---
+++ app/interactors/responses/add_mark_id_after_save.rb
@@ -0,0 +1,12 @@
+module Responses
+    class AddMarkIdAfterSave
+        include Interactor
+
+        def call(response: context.response, mark_id: context.mark.id)
+            response.update!(mark_id: mark_id)
+            context.response = response
+        rescue ActiveRecord::RecordInvalid => e
+            context.fail!(error: e.message)
+        end
+    end
+end
