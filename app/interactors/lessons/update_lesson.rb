0b3186bb836af6b99418d6a8e73145c8fc5bda2f
tetoraillia <teterailiya@gmail.com>
2025-07-16 12:21:23 +0300

    feat: Add a method for update lesson.is_open? after successfull save

--- app/interactors/lessons/update_lesson.rb
+++ app/interactors/lessons/update_lesson.rb
@@ -6,8 +6,10 @@ module Lessons
 
         def call
             update_lesson(context.id, context.params)
+            check_lesson_open_status
 
             if @lesson.save
+
                 context.lesson = @lesson
             else
                 context.fail!(error: @lesson.errors.full_messages.to_sentence)
@@ -20,6 +22,10 @@ module Lessons
             if context.id.nil? || context.params.nil? || context.course.nil? || context.topic.nil?
                 context.fail!(error: "Invalid context")
             end
+        end
+
+        def check_lesson_open_status
+            @lesson.update!(is_open: true) if @lesson.ends_at > Time.current
         end
 
         def update_lesson(id, params)
