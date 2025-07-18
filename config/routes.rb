ad18b727b67c0aa177ec613c5dee82e757dcfda1
tetoraillia <teterailiya@gmail.com>
2025-07-18 09:56:56 +0300

    fix: Add update and show methods for responses

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
       end
     end
     resources :lessons do
-      resources :responses, only: [ :create ]
+      resources :responses, only: [ :create, :update, :show ]
     end
     resources :invitations, only: [ :new, :create ] do
       member do
