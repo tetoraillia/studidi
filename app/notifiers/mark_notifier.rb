58cac47b55bcdb5f1aa40ba314640e094c167baa
Setoju <setoju48@gmail.com>
2025-07-22 11:44:54 +0300

    refactor: Simplify mark notifier

--- app/notifiers/mark_notifier.rb
+++ app/notifiers/mark_notifier.rb
@@ -3,25 +3,11 @@
 # MarkNotifier.with(record: @post, message: "New post").deliver(User.all)
 
 class MarkNotifier < ApplicationNotifier
-  recipients -> { params[:recipient] }
-
-  deliver_by :database
   deliver_by :action_cable,
             stream: -> { "notifications_#{recipient.id}" },
             message: -> { params[:message] },
             url: -> { params[:url] },
             id: -> { notification.id }
-
-  required_param :message
-  required_param :url
-
-  def message
-    params[:message]
-  end
-
-  def url
-    params[:url]
-  end
   # Add your delivery methods
   #
   # deliver_by :email do |config|
