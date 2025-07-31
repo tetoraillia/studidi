7c0eb3006c1e161772372a1b6ead2e63248fba34
Setoju <setoju48@gmail.com>
2025-07-31 17:40:45 +0300

    test: Test the ui for course enrollment

---
+++ spec/system/enrollment_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'Course enrollment', type: :system do
+  let(:course_owner) { create(:user) }
+  let(:student) { create(:user) }
+  let!(:course) { create(:course, instructor: course_owner) }
+
+  before do
+    driven_by(:rack_test)
+    system_login_as(student)
+  end
+
+  it 'allows a user to enroll in a course' do
+    visit course_path(course)
+
+    expect(page).to have_content(course.title)
+
+    click_button 'Enroll'
+
+    expect(page).to have_content('Successfully enrolled in the course')
+    expect(student.enrolled_in?(course)).to be_truthy
+  end
+end
